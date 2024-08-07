e18ff8b4bd2287bb223a32d28fb42fa33348133d
chjaved <chjaved649@gmail.com>
2024-08-07 09:46:30 -0700

    Next added

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_ZEGO_APP_ID: "2017108866", // Change this to a string
+      NEXT_PUBLIC_ZEGO_SERVER_ID: "478dae90644e0d69b9838aef2662d202",
+    },
+    reactStrictMode: false,
+    images: {
+      domains: ["localhost"],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
